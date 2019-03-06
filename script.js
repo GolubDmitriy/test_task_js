56a50a151f1f362277c3c02601d74bb715c15b77
unknown <golub.d.a@yandex.ru>
2019-03-06 22:38:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,115 @@
+window.onload = function() {
+    const choice = document.getElementsByClassName('choice');
+    
+    const btn = document.getElementById('btn');
+
+    const help = document.getElementById('help');
+
+    let totalChoiceActive = 0;
+
+    let answers = {};
+
+    for (let elem of choice) {
+        elem.addEventListener('click', choiceFunc);
+    }
+
+    function choiceFunc() {
+
+        const elem = event.target;
+        
+        if (!~elem.classList.value.indexOf('active-choice')) {
+            elem.classList.add('active-choice');
+            btn.classList.add('active-btn');
+            btn.addEventListener('click', func);
+            answers[elem.id] = true;
+            ++totalChoiceActive;
+        } else {
+            elem.classList.remove('active-choice');
+            answers[elem.id] = false;
+            if (!--totalChoiceActive) {
+                btn.classList.remove('active-btn');
+                btn.removeEventListener('click', func);
+            }
+        }
+    }
+
+    function func() {
+        if (answers.answer1 === true &&
+            answers.answer3 === true &&
+            (!answers.answer4 || answers.answer4 === false) && 
+            (!answers.answer2 || answers.answer2 === false)) {
+                    
+            btn.classList.add('correctly-btn');
+            removeAllListener();
+            hiddenHelp();
+            setTimeout(completeClear, 1500);
+
+        } else if (answers.answer2 === true && answers.answer4 === true) {
+            reload(2, 4);
+        } else if (answers.answer2 === true) {
+            reload(2);
+        } else if (answers.answer4 === true) {
+            reload(4);
+        } else if (answers.answer1 === true) {
+            reload();
+        } else {
+            reload();
+        }
+    }
+          
+    function completeClear() {
+        const main = document.getElementById('main');
+        main.classList.add('complete-clear');
+    }
+
+    function wrongAnswer(number) {
+        if (number) {
+            const answer = 'answer' + number;
+            document.getElementById(answer).classList.add('error');
+        }
+        btn.classList.add('error');
+    } 
+
+    function removeAllListener() {
+        hiddenHelp();
+        btn.removeEventListener('click', func);
+        for (let elem of choice) {
+            elem.removeEventListener('click', choiceFunc);
+        }
+    }
+
+    function refresh() {
+        changeHelp();
+        totalChoiceActive = 0;
+        answers = {};
+        btn.classList.remove('error');
+        btn.classList.remove('active-btn');
+        for (let elem of choice) {
+            elem.classList.remove('error');
+            elem.classList.remove('active-choice');
+            elem.addEventListener('click', choiceFunc);
+        }
+    }
+
+    function changeHelp() {
+        if (answers.answer2 === true || answers.answer4 === true) {
+            help.innerHTML = 'Вычисли Х';
+        } else {
+            help.innerHTML = 'Это не все правильные ответы';
+        }
+        help.style.visibility = 'visible';
+    }
+
+    function hiddenHelp() {
+        help.style.visibility = 'hidden';
+    }
+
+    function reload(num1, num2) {
+        if (num2) {
+            wrongAnswer(num2);
+        }
+        wrongAnswer(num1);
+        removeAllListener();
+        setTimeout(refresh, 1000);
+    }
+}
